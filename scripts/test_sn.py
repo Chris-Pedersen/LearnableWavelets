91b6a5439e29f7bcb526e5d24c5edb8ecea01047
Chris-Pedersen <christian.pedersen.17@ucl.ac.uk>
2022-01-06 13:21:18 +0000

    Script to test a scattering network

---
+++ scripts/test_sn.py
@@ -0,0 +1,174 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.backends.cudnn as cudnn
+from torch.utils.data.dataset import Dataset
+from torch.utils.data import DataLoader
+import time, sys, os
+import matplotlib.pyplot as plt
+
+# my modules
+from sn_camels.models.models_factory import baseModelFactory, topModelFactory
+from sn_camels.models.sn_hybrid_models import sn_HybridModel
+from sn_camels.camels.camels_dataset import *
+
+
+""" Base script to test a scattering network on a CAMELs dataset """
+
+
+## First create a scattering network object
+scatteringBase = baseModelFactory( #creat scattering base model
+    architecture='scattering',
+    J=2,
+    N=256,
+    M=256,
+    second_order=True,
+    initialization="Random",
+    seed=123,
+    learnable=False,
+    lr_orientation=0.1,
+    lr_scattering=0.1,
+    filter_video=False,
+    device="cpu",
+    use_cuda=False
+)
+
+## Now create a network to follow the scattering layers
+## can be MLP, linear, or cnn at the moment
+## (as in https://github.com/bentherien/ParametricScatteringNetworks/ )
+top = topModelFactory( #create cnn, mlp, linearlayer, or other
+    base=scatteringBase,
+    architecture="cnn",
+    num_classes=12, 
+    width=8, 
+    use_cuda=False
+)
+
+## Merge these into a hybrid model
+hybridModel = sn_HybridModel(scatteringBase=scatteringBase, top=top, use_cuda=False)
+
+## Check if CUDA available
+if torch.cuda.is_available():
+    print("CUDA Available")
+    device = torch.device('cuda')
+else:
+    print('CUDA Not Available')
+    device = torch.device('cpu')
+cudnn.benchmark = True      #May train faster but cost more memory
+
+
+#######################################################################################################
+#######################################################################################################
+############################## Set up training params #################################################
+#######################################################################################################
+#######################################################################################################
+## camels path
+camels_path=os.environ['CAMELS_PATH']
+
+# data parameters
+fmaps      = ['maps_Mcdm.npy'] #tuple containing the maps with the different fields to consider
+fmaps_norm = [None] #if you want to normalize the maps according to the properties of some data set, put that data set here (This is mostly used when training on IllustrisTNG and testing on SIMBA, or vicerversa)
+fparams    = camels_path+"/params_IllustrisTNG.txt"
+seed       = 1   #random seed to split maps among training, validation and testing
+splits     = 6   #number of maps per simulation
+
+# training parameters
+channels        = 1                #we only consider here 1 field
+params          = [0,1,2,3,4,5]    #0(Omega_m) 1(sigma_8) 2(A_SN1) 3 (A_AGN1) 4(A_SN2) 5(A_AGN2). The code will be trained to predict all these parameters.
+g               = params           #g will contain the mean of the posterior
+h               = [6+i for i in g] #h will contain the variance of the posterior
+rot_flip_in_mem = False            #whether rotations and flipings are kept in memory. True will make the code faster but consumes more RAM memory.
+
+# optimizer parameters
+beta1 = 0.5
+beta2 = 0.999
+
+# hyperparameters
+batch_size = 128
+lr         = 1e-3
+wd         = 0.0005  #value of weight decay
+dr         = 0.2    #dropout value for fully connected layers
+hidden     = 5      #this determines the number of channels in the CNNs; integer larger than 1
+epochs     = 10    #number of epochs to train the network
+
+# output files names
+floss  = 'loss.txt'   #file with the training and validation losses for each epoch
+fmodel = 'weights.pt' #file containing the weights of the best-model
+#######################################################################################################
+#######################################################################################################
+
+
+fmaps2 = camels_path+"/Maps_Mcdm_IllustrisTNG_LH_z=0.00.npy"
+maps  = np.load(fmaps2)
+print('Shape of the maps:',maps.shape)
+# define the array that will contain the indexes of the maps
+indexes = np.zeros(1000*splits, dtype=np.int32)
+
+# do a loop over all maps and choose the ones we want
+count = 0
+for i in range(15000):
+    if i%15 in np.arange(splits):  
+      indexes[count] = i
+      count += 1
+print('Selected %d maps out of 15000'%count)
+
+# save these maps to a new file
+maps = maps[indexes]
+np.save('maps_Mcdm.npy', maps)
+del maps
+
+# This routine returns the data loader need to train the network
+def create_dataset_multifield(mode, seed, fmaps, fparams, batch_size, splits, fmaps_norm,
+                              rot_flip_in_mem=True, shuffle=True, verbose=False):
+
+    # whether rotations and flippings are kept in memory
+    if rot_flip_in_mem:
+        data_set = make_dataset_multifield(mode, seed, fmaps, fparams, splits, fmaps_norm, verbose)
+    else:
+        data_set = make_dataset_multifield2(mode, seed, fmaps, fparams, splits, fmaps_norm, verbose)
+
+    data_loader = DataLoader(dataset=data_set, batch_size=batch_size, shuffle=shuffle)
+    return data_loader
+
+
+# get training set
+print('\nPreparing training set')
+train_loader = create_dataset_multifield('train', seed, fmaps, fparams, batch_size, splits, fmaps_norm, 
+                                         rot_flip_in_mem=rot_flip_in_mem, verbose=True)
+
+# get validation set
+print('\nPreparing validation set')
+valid_loader = create_dataset_multifield('valid', seed, fmaps, fparams, batch_size, splits, fmaps_norm, 
+                                         rot_flip_in_mem=True,  verbose=True)    
+
+
+model = hybridModel
+#model = model_o3_err(hidden, dr, channels)
+model.to(device=device)
+
+optimizer = torch.optim.AdamW(model.parameters(), lr=lr, weight_decay=wd, betas=(beta1, beta2))
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', factor=0.3, patience=10)
+
+print('Computing initial validation loss')
+model.eval()
+valid_loss1, valid_loss2 = torch.zeros(len(g)).to(device), torch.zeros(len(g)).to(device)
+min_valid_loss, points = 0.0, 0
+for x, y in valid_loader:
+      with torch.no_grad():
+          bs   = x.shape[0]                #batch size
+          x    = x.to(device=device)       #maps
+          y    = y.to(device=device)[:,g]  #parameters
+          p    = model(x)                  #NN output
+          y_NN = p[:,g]                    #posterior mean
+          e_NN = p[:,h]                    #posterior std
+          loss1 = torch.mean((y_NN - y)**2,                axis=0)
+          loss2 = torch.mean(((y_NN - y)**2 - e_NN**2)**2, axis=0)
+          loss  = torch.mean(torch.log(loss1) + torch.log(loss2))
+          valid_loss1 += loss1*bs
+          valid_loss2 += loss2*bs
+          points += bs
+min_valid_loss = torch.log(valid_loss1/points) + torch.log(valid_loss2/points)
+min_valid_loss = torch.mean(min_valid_loss).item()
+print('Initial valid loss = %.3e'%min_valid_loss)
+
+
