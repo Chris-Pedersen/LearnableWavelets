e0d59d663f2ff5f6084f1148bd40a0a0c5661344
Chris-Pedersen <c.pedersen@nyu.edu>
2022-04-06 17:48:50 -0400

    Updated camels optuna script

--- scripts/optuna_scripts/camels_optuna.py
+++ scripts/optuna_scripts/camels_optuna.py
@@ -10,8 +10,10 @@ import matplotlib.pyplot as plt
 # my modules
 from sn_camels.models.models_factory import baseModelFactory, topModelFactory
 from sn_camels.models.sn_hybrid_models import sn_HybridModel
+from sn_camels.models.camels_models import get_architecture
 from sn_camels.models.camels_models import model_o3_err
 from sn_camels.camels.camels_dataset import *
+
 
 import optuna
 import wandb
@@ -19,21 +21,22 @@ import wandb
 ## Everything done within an Objective class now for optuna
 class Objective(object):
     def __init__(self, device, seed, fmaps, fmaps_norm, fparams, batch_size, splits,
-                      arch, min_lr, beta1, beta2, epochs, monopole,
+                      arch, hidden, beta1, beta2, epochs, monopole, name,
                       num_workers, params, rot_flip_in_mem, smoothing):
         self.device          = device
         self.seed            = seed
-        self.fmaps          = fmaps
-        self.fmaps_norm     = fmaps_norm
-        self.fparams        = fparams
+        self.fmaps           = fmaps
+        self.fmaps_norm      = fmaps_norm
+        self.fparams         = fparams
         self.batch_size      = batch_size
         self.splits          = splits
         self.arch            = arch
-        self.min_lr          = min_lr
+        self.hidden          = hidden
         self.beta1           = beta1
         self.beta2           = beta2
         self.epochs          = epochs
         self.monopole        = monopole
+        self.name            = name
         self.num_workers     = num_workers
         self.params          = params
         self.rot_flip_in_mem = rot_flip_in_mem
@@ -54,42 +57,27 @@ class Objective(object):
         wd     = trial.suggest_float("wd", 1e-8, 1e-1, log=True)
         dr     = trial.suggest_float("dr", 0.0,  0.9)
         print("Suggested trial")
-        ## Store hyperparams in a config for wandb
-        config = {"learning rate": lr,
-                 "epochs": self.epochs,
-                 "batch size": self.batch_size,
-                 "network": self.arch,
-                 "weight decay": wd,
-                 "dropout": dr,
-                 "splits":self.splits}
 
         print('\nTrial number: {}'.format(trial.number))
         print('lr: {}'.format(lr))
         print('wd: {}'.format(wd))
         print('dr: {}'.format(dr))
-
-
-        ## Architecture-specific params
-        if self.arch=="sn":
-            lr_orient = trial.suggest_float("lr_orient",1e-4,1,log=True)
-            lr_scat   = trial.suggest_float("lr_scat",1e-4,1,log=True)
-
-            config["lr_orient"]=lr_orient
-            config["lr_scat"]=lr_scat
-            print('lr_orient: {}'.format(lr_orient))
-            print('lr_scat: {}'.format(lr_scat))
-        else:
-            hidden = trial.suggest_int("hidden", 4, 12)
-            config["hidden"]=hidden
-            print('hidden: {}'.format(hidden))
+        
+        config = {"learning rate": lr,
+                 "epochs": self.epochs,
+                 "batch size": self.batch_size,
+                 "network": self.arch,
+                 "dropout": dr,
+                 "splits": self.splits}
+    
 
         ## Initialise wandb
         wandb.login()
-        wandb.init(project="optuna camels 1kmaps sbatch", entity="chris-pedersen",config=config)
+        wandb.init(project="%s" % name, entity="chris-pedersen",config=config)
 
         ### LOAD DATA
         ## camels path
-        camels_path=os.environ['CAMELS_PATH']
+        camels_path="/mnt/ceph/users/camels/PUBLIC_RELEASE/CMD/2D_maps/data/"
 
         # get training set
         print('\nPreparing training set')
@@ -103,47 +91,9 @@ class Objective(object):
 
         num_train_maps=train_loader.dataset.x.size
         wandb.config.update({"no. training maps": num_train_maps})
-
-        if self.arch=="sn":
-            ## First create a scattering network object
-            scatteringBase = baseModelFactory( #creat scattering base model
-                architecture='scattering',
-                J=2,
-                N=256,
-                M=256,
-                second_order=True,
-                initialization="Random",
-                seed=123,
-                learnable=True,
-                lr_orientation=0.1,
-                lr_scattering=0.1,
-                filter_video=False,
-                device=device,
-                use_cuda=True
-            )
-
-            wandb.config.update({"learnable":scatteringBase.learnable})
-            ## Now create a network to follow the scattering layers
-            ## can be MLP, linear, or cnn at the moment
-            ## (as in https://github.com/bentherien/ParametricScatteringNetworks/ )
-            top = topModelFactory( #create cnn, mlp, linearlayer, or other
-                base=scatteringBase,
-                architecture="cnn",
-                num_classes=12,
-                width=8,
-                use_cuda=True
-            )
-            
-
-            ## Merge these into a hybrid model
-            hybridModel = sn_HybridModel(scatteringBase=scatteringBase, top=top, use_cuda=use_cuda)
-            model=hybridModel
-            print("scattering layer + cnn set up")
-        elif self.arch=="camels":
-            model = model_o3_err(hidden, dr, channels)
-            print("camels cnn model set up")
-        else:
-            print("model type %s not recognised" % self.arch)
+        print("setting up model %s" % self.arch)
+        model = get_architecture(self.arch,self.hidden,dr,channels)
+        wandb.config.update({"learnable_parameters":sum(p.numel() for p in model.parameters())})
         model.to(device=device)
 
         # wandb
@@ -203,9 +153,6 @@ class Objective(object):
             train_loss = torch.log(train_loss1/points) + torch.log(train_loss2/points)
             train_loss = torch.mean(train_loss).item()
 
-            
-            wandb.log({"training loss": train_loss})
-
             # do validation: cosmo alone & all params
             valid_loss1, valid_loss2 = torch.zeros(len(g)).to(device), torch.zeros(len(g)).to(device)
             valid_loss, points = 0.0, 0
@@ -228,7 +175,7 @@ class Objective(object):
             valid_loss = torch.mean(valid_loss).item()
 
             scheduler.step(valid_loss)
-            wandb.log({"validation loss": valid_loss})
+            wandb.log({"training loss": train_loss, "validation loss": valid_loss})
 
 
             # verbose
@@ -258,7 +205,8 @@ beta2 = 0.999
 
 ## My stuff
 ## camels path
-camels_path=os.environ['CAMELS_PATH']
+name       ="optuna_camels_deep"
+camels_path="/mnt/ceph/users/camels/PUBLIC_RELEASE/CMD/2D_maps/data/"
 fparams    = camels_path+"/params_IllustrisTNG.txt"
 fmaps      = ['maps_Mcdm.npy']
 fmaps_norm = [None]
@@ -266,9 +214,9 @@ splits     = 1
 seed       = 123
 params     = [0,1,2,3,4,5] #0(Om) 1(s8) 2(A_SN1) 3 (A_AGN1) 4(A_SN2) 5(A_AGN2)
 monopole        = True  #keep the monopole of the maps (True) or remove it (False)
-rot_flip_in_mem = True  #whether rotations and flipings are kept in memory
+rot_flip_in_mem = False  #whether rotations and flipings are kept in memory
 smoothing  = 0  ## Smooth the maps with a Gaussian filter? 0 for no
-arch = "camels" ## Which model architecture to use    
+arch = "o3_err" ## Which model architecture to use    
 
 
 fmaps2 = camels_path+"/Maps_Mcdm_IllustrisTNG_LH_z=0.00.npy"
@@ -279,7 +227,7 @@ indexes = np.zeros(1000*splits, dtype=np.int32)
 
 # do a loop over all maps and choose the ones we want
 count = 0
-for i in range(15000):
+for i in range(5000):
     if i%15 in np.arange(splits):  
       indexes[count] = i
       count += 1
@@ -291,19 +239,19 @@ np.save('maps_Mcdm.npy', maps)
 del maps
 
 ## training parameters
-batch_size  = 128
-min_lr      = 1e-9
+batch_size  = 32
+hidden      = 5
 epochs      = 100
 num_workers = 1    #number of workers to load data
 
 ## Optuna params
-study_name = "optuna/camels-test"  # Unique identifier of the study.
+study_name = "optuna/"+name  # Unique identifier of the study.
 storage_name = "sqlite:///{}.db".format(study_name)
-n_trials=20
+n_trials=35
 
 # train networks with bayesian optimization
 objective = Objective(device, seed, fmaps, fmaps_norm, fparams, batch_size, splits,
-                      arch, min_lr, beta1, beta2, epochs, monopole, 
+                      arch, hidden, beta1, beta2, epochs, monopole, name,
                     num_workers, params, rot_flip_in_mem, smoothing)
 sampler = optuna.samplers.TPESampler(n_startup_trials=20)
 study = optuna.create_study(study_name=study_name, sampler=sampler, storage=storage_name,
